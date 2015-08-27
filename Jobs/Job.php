69217c72b336fcdf2e311a8ee6fa4e255a07bf37
anonymframework <serif.saglam119@outlook.com>
2015-08-27 16:16:42 +0300

    made some updates

--- Jobs/Job.php
+++ Jobs/Job.php
@@ -18,4 +18,159 @@ namespace Anonym\Components\Cron\Job;
 class Job
 {
 
+    /**
+     * the minute of cronjob
+     *
+     * @var string
+     */
+    private $minute = '*';
+
+    /**
+     * the hour of cronjob
+     *
+     * @var string
+     */
+    private $hour = '*';
+
+    /**
+     * the day of mount
+     *
+     * @var string
+     */
+    private $dayOfMounth = '*';
+
+    /**
+     * the mounth of cronjob
+     *
+     * @var string
+     */
+    private $mounth = '*';
+
+    /**
+     * the day of week
+     *
+     * @var string
+     */
+    private $dayOfWeek = '*';
+
+    /**
+     * the year of cronjob
+     *
+     * @var string
+     */
+    private $year;
+
+    /**
+     * @return string
+     */
+    public function getMinute()
+    {
+        return $this->minute;
+    }
+
+    /**
+     * @param string $minute
+     * @return Job
+     */
+    public function setMinute($minute)
+    {
+        $this->minute = $minute;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHour()
+    {
+        return $this->hour;
+    }
+
+    /**
+     * @param string $hour
+     * @return Job
+     */
+    public function setHour($hour)
+    {
+        $this->hour = $hour;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDayOfMounth()
+    {
+        return $this->dayOfMounth;
+    }
+
+    /**
+     * @param string $dayOfMounth
+     * @return Job
+     */
+    public function setDayOfMounth($dayOfMounth)
+    {
+        $this->dayOfMounth = $dayOfMounth;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMounth()
+    {
+        return $this->mounth;
+    }
+
+    /**
+     * @param string $mounth
+     * @return Job
+     */
+    public function setMounth($mounth)
+    {
+        $this->mounth = $mounth;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDayOfWeek()
+    {
+        return $this->dayOfWeek;
+    }
+
+    /**
+     * @param string $dayOfWeek
+     * @return Job
+     */
+    public function setDayOfWeek($dayOfWeek)
+    {
+        $this->dayOfWeek = $dayOfWeek;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param string $year
+     * @return Job
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+
+        return $this;
+    }
 }
