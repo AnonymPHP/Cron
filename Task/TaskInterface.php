4838d452f35738a79af525cf99c4f896356944f7
anonymframework <serif.saglam119@outlook.com>
2015-08-28 09:16:13 +0300

    added setCommand

--- Task/TaskInterface.php
+++ Task/TaskInterface.php
@@ -15,4 +15,12 @@ namespace Anonym\Components\Cron\Task;
 interface TaskInterface
 {
 
+    /**
+     * register the command
+     *
+     * @param mixed $command
+     * @return $this
+     */
+    public function setCommand($command);
+
 }
