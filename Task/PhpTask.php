6e24f60dd927d0914c9457f5009231567f41f670
anonymframework <serif.saglam119@outlook.com>
2015-08-28 07:56:24 +0300

    added set command function

--- Task/PhpTask.php
+++ Task/PhpTask.php
@@ -18,4 +18,28 @@ namespace Anonym\Components\Cron\Task;
 class PhpTask extends Task
 {
 
+    /**
+     * get the php.exe path
+     *
+     * @return string
+     */
+    private function  resolvePhpInstalledPath()
+    {
+        if (null !== $path = exec('which php')) {
+            return $path;
+        }
+
+        return '/usr/bin/php';
+    }
+
+    /**
+     * register the command
+     *
+     * @param string $command
+     * @return Task
+     */
+    public function setCommand($command)
+    {
+        $this->command = $this->resolvePhpInstalledPath().' '.$command;
+    }
 }
