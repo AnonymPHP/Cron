43eb63740c0de4c98de05ca29b700e6e315577d2
anonymframework <serif.saglam119@outlook.com>
2015-08-28 09:43:22 +0300

    added call method

--- Cron.php
+++ Cron.php
@@ -10,12 +10,23 @@
 
 
 namespace Anonym\Components\Cron;
-
+use Closure;
 /**
  * Class Cron
  * @package Anonym\Components\Cron
  */
 class Cron
 {
+
+    public function call(Closure $command)
+    {
+        $response = $command();
+        if($this->resolveCommandResponse($response))
+        {
+            EventReposity::add($response);
+        }
+    }
+
+
 
 }
