6d8f23dc9392caee474626f795e201ef4fb53fad
anonymframework <serif.saglam119@outlook.com>
2015-08-28 08:02:14 +0300

    added set command method

--- Task/ConsoleTask.php
+++ Task/ConsoleTask.php
@@ -14,5 +14,16 @@ namespace Anonym\Components\Cron\Task;
 
 class ConsoleTask extends PhpTask
 {
+    /**
+     * register the command
+     *
+     * @param string $command
+     * @return Task
+     */
+    public function setCommand($command)
+    {
+        $this->command = 'anonym '.$command;
 
+        return $this;
+    }
 }
