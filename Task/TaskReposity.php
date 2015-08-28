435b485d70c3fccc58d353cf8ff84b8f73984620
anonymframework <serif.saglam119@outlook.com>
2015-08-28 07:58:25 +0300

    added task reposity

--- Task/TaskReposity.php
+++ Task/TaskReposity.php
@@ -14,5 +14,33 @@ namespace Anonym\Components\Cron\Task;
 
 class TaskReposity
 {
+    /**
+     * the command for job
+     *
+     * @var string
+     */
+    protected $command;
+
+    /**
+     * @return string
+     */
+    public function getCommand()
+    {
+        return $this->command;
+    }
+
+    /**
+     * register the command
+     *
+     * @param string $command
+     * @return Task
+     */
+    public function setCommand($command)
+    {
+        $this->command = $command;
+
+        return $this;
+    }
+
 
 }
