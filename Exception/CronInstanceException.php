4d359b1df13b2f403ba22605824bd27ff244be44
anonymframework <serif.saglam119@outlook.com>
2015-08-29 07:15:11 +0300

    made some updates

--- Exception/CronInstanceException.php
+++ Exception/CronInstanceException.php
@@ -12,7 +12,21 @@
 namespace Anonym\Components\Cron\Exception;
 use Exception;
 
+
+/**
+ * Class CronInstanceException
+ * @package Anonym\Components\Cron\Exception
+ */
 class CronInstanceException extends Exception
 {
 
+    /**
+     * throw the exception
+     *
+     * @param string $message
+     */
+    public function __construct($message = '')
+    {
+        $this->message = $message;
+    }
 }
