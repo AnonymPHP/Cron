d2b55c01ce99e6d9e2768f3a8832e496359d8387
anonymframework <serif.saglam119@outlook.com>
2015-08-29 15:01:57 +0300

    made some updates

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ include 'vendor/autoload.php';
 $basic = new \Anonym\Components\Cron\BasicCron();
 
 $basic->event(function(){
-   return \Anonym\Components\Cron\Task\Task::exec('echo foo')->daily();
+   return \Anonym\Components\Cron\Task\Task::file('/var/www/html/cron/test.php')->everyMinute();
 });
 
 $basic->run();
